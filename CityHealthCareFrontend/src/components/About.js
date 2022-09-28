9eaf9d59898a7cf863b2f2282f7738e6dd32f244
CityHealthCare <114415953+CityHealthCare@users.noreply.github.com>
2022-09-28 16:20:43 +0530

    Update About.js

--- CityHealthCareFrontend/src/components/About.js
+++ CityHealthCareFrontend/src/components/About.js
@@ -14,19 +14,19 @@ export default function About() {
         <h2 className="text-start pb-2 mt-2">About Us</h2>
         <h3>City Health Care Portal</h3>
         <p >
-          This project city health care portal aims at to develop the software
+          This project city health care portal aims at to develop the application
           that covers all the aspects of management and operations of hospital.
           So, with the victim and proof of corona pandemic situation we are
           introducing this project. So, if further in future again pandemic
-          situation like corona comes into human life at that time this software
+          situation like corona comes into human life at that time this application
           will help the peoples a lot in the procedure of finding facilities.
-          This software enables the health providers to provide the operational
+          This application enables the health providers to provide the operational
           health care availability, reduce time consumption and enhance delivery
           of quality of care. The main purpose of our project is to make
           hospital related task easy and saves the time of public while
           searching health care facilities. This project maintains helps to
           maintains the details of hospital related queries (like oxygen
-          availability, Bed availability etc.) With this software people can get
+          availability, Bed availability etc.) With this application people can get
           the information of nearby hospital location wise wherever they are.
         </p>
         <hr />
